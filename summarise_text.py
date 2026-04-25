f791cf1ee387efadb25058d46a24ea17c3f9fd73
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2026-04-25 16:34:08 +0100

    Update code to send text to Gemini for summarisation

--- summarise_text.py
+++ summarise_text.py
@@ -59,6 +59,13 @@ async def summarise(file: UploadFile = File(...)):
     if text.strip():
         return {"error": "Could not extract and readable text from this file"}
     
+    # Request - Send the clean text to Gemini API
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        content=f"Summarise the following document accurately:\n{text}"
+    )
+    
+    return {"summarise": response.text}
    
             
     
