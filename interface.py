4e361d106df22e30e20f20bc232678646516d6fe
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2026-05-26 20:23:08 +0100

    Rename setting.py to config.py to fix server error

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-from setting import settings
+from config import settings
 
 API_URL = settings.API_URL
 
--- resume_reviewer.py
+++ resume_reviewer.py
@@ -6,7 +6,7 @@ from google.genai import types
 from docx import Document
 
 import stripe
-from setting import settings
+from config import settings
 
 # Initialize the Gemini Client
 client = genai.Client(api_key=settings.GEMINI_API_KEY.get_secret_value())
